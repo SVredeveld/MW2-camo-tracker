81659399902e21012dbd84b87f3479a8f044935c
SVredeveld <sonjavredeveld@gmail.com>
2022-10-26 23:22:56 +0200

    First ts changes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,71 @@
 import './style.scss';
 
-function helloWorld() {
-  const element= document.getElementById("hello-world");
-  
+let weapon_ar: string[] = ["stg", "volk", "scarrr", "newweapon"];
+
+interface Challenge {
+	description: string;
+	completed: boolean;
+}
+
+interface Weapon {
+	id: string;
+	name: string;
+	challenges: Challenge[];
+	completed: boolean;
+}
+
+function getWeaponId() {
+	return 'Random string';
+}
+
+function getChallengesByWeaponType(): Challenge {
+	return {
+		description: "test",
+		completed: true,
+	}
+}
+
+function addNewWeapon (name: string, type: string) {
+	return {
+		id: getWeaponId(),
+		name,
+		challenges: getChallengesByWeaponType()
+	};
+}
+
+function CreateARs(weapon: string[]) {
+  const element= document.getElementById("weapons-list-ar");
+	
   if (element) {
-    element.textContent = "Hello, World!";
-		element.style.color = 'red';
+		weapon.forEach(w => {
+			let arTitle = "ar-title-"+w;
+			element.innerHTML += `
+				<li id="${arTitle}"> 
+					${w}
+				</li>
+			`;
+			const title = document.getElementById(arTitle);
+			if(title) {
+				console.log(arTitle)
+				title.textContent = w;
+				CreateChallengeslist(arTitle);
+			}
+		});
+
+	
   }
 }
-helloWorld();
+
+let challenges_ar: string[] = ["headshots", "longshots", "500 kills"]
+
+function CreateChallengeslist(title: string) {
+ const element = document.getElementById(title);
+ console.log(element)
+
+ element?.innerHTML += `
+	<p>${challenges_ar}</p>
+ `
+}
+CreateARs(weapon_ar);
+
 
