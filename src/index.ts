13412218f9544c223fc0607daeac04abe64d6f96
SVredeveld <sonjavredeveld@gmail.com>
2022-10-26 21:57:54 +0200

    plz work now

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import './style.scss';
 
-
 function helloWorld() {
   const element= document.getElementById("hello-world");
   
   if (element) {
     element.textContent = "Hello, World!";
-		element.style.color = 'yellow';
+		element.style.color = 'red';
   }
 }
 helloWorld();
